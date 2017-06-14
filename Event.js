827f08a2cf01a573f999d50207b158126b894d40
suifeng123 <wangshengwen31@gmail.com>
2017-06-14 18:58:53 +0800

    Thinking-in-javaScript

---
+++ Event.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+var Event = (function(){
+    var clientList = {};
+    var listen;
+    var trigger;
+    var remove;
+    listen = function(key,fn){
+        if(!clientList[key]){
+            clientList[key] = [];
+        }
+        clientList[key].push(fn);
+    };
+    trigger = function() {
+        var key = Array.prototype.shift.call(arguments);
+        var fns = clientList[key];
+        if(!fns || fns.length === 0){
+            return false
+        }
+        for(var i= 0,fn;fn=fns[i++];){
+            fn.apply(this.arguments);
+        }
+    };
+
+    remove = function(key,fn){
+        var fns = clientList[key];
+        if(!fns){
+            return false
+        }
+        if(!fn){
+            fns && (fns.length = 0)
+        }else {
+            for(var l=fns.length-1;l>=0;l--){
+                var _fn = fns[l];
+                if(_fn === fn){
+                    fns.splice(l,1);
+                }
+            }
+        }
+    };
+    return {
+        listen: listen,
+        trigger: trigger,
+        remove: remove
+    }
+})();
+
+Event.listen('squareMeter88',function(price){
+    console.log("价格="+price);
+}); //小红订阅的消息
+
+Event.trigger('squareMeter88',20000);
