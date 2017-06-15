c616c65486ab9c4827fb8f886a327058a3e0c6c3
suifeng123 <wangshengwen31@gmail.com>
2017-06-15 19:07:38 +0800

    Thinking-in-javaScript

---
+++ Hong.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Administrator on 2017/6/15.
+ */
+var closeDoorCommand = {
+    execute: function(){
+        console.log("执行关门的操作");
+    }
+};
+
+var openPcCommand = {
+    execute: function(){
+        console.log("打开电脑");
+    }
+};
+
+var openQQCommand =  {
+    execute: function() {
+        console.log("打开QQ应用");
+    }
+};
+
+var MacroCommand = function() {
+    return {
+        commandList:[],
+        add: function(command){
+            this.commandList.push(command);
+        },
+        execute: function() {
+            for(var i= 0,command;command=this.commandList[i++];){
+                command.execute();
+            }
+        }
+
+    }
+
+};
+
+var aa = MacroCommand();
+aa.add(openPcCommand);
+aa.add(openQQCommand);
+aa.add(closeDoorCommand);
+
+aa.execute();
