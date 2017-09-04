5656ca9c732a64082e1368bfe8c311868552184e
suifeng123 <wswen1220@163.com>
2017-09-04 16:32:40 +0800

    贪心算法

--- beibao.js
+++ beibao.js
@@ -2,8 +2,28 @@ function max(a,b){
 	return (a > b)? a : b;
 }
 
-function knapsack(capacity,size,value,n){
-	if(n == 0 || capacity == 0){
-		return 0;
+function dKnapsack(capacity,size,value,n){
+	var K = [];
+	for(var i = 0 ; i <= capacity + 1; i++){
+		K[i] = [];
 	}
+	for(var i = 0 ; i < n ; i++){
+		for(var w = 0; w <= capacity; w++){
+			if(i == 0 || w == 0){
+				K[i][w] = 0; //
+			}else if(size[i-1] <= w){
+				K[i][w] = max(value[i-1]+K[i-1][w-size[i-1]],K[i-1][w]);
+			}else{
+				K[i][w] = K[i-1][w];
+			}
+			console.log(K[i][w]);
+		}
+	}
+	return K[n][capacity];
 }
+
+var value = [4,5,10,11,13];
+var size = [3,4,7,8,9];
+var capacity = 16;
+var n = 5;
+console.log(dKnapsack(capacity,size,value,n));
---
+++ tanxin2.js
@@ -0,0 +1,6 @@
+function ksack(values,weights,capacity){
+	//使用贪心算法来计算
+	var load = 0;
+	var i = 0;
+	var w = 0;
+}
