6ee1001e939ce575c7a1e7a9fbb41759f6811b60
suifeng123 <wswen1220@163.com>
2017-10-16 18:11:27 +0800

    提交css属性的页面

--- Object_define.js
+++ Object_define.js
@@ -1,10 +1,35 @@
 //使用definePrototype
-var person = {};
-Object.defineProperty(person,"name",{
-	writable:false,
-	value:'wangshengwen'
+var book = {};
+
+Object.defineProperties(book,{
+	_year:{
+		value: 2004,
+	},
+	edition: {
+		value: 1
+	},
+	year:{
+		get: function(){
+			return this._year;
+		},
+		set: function(newValue){
+			if(newValue > 2004){
+				this._year = newValue;
+				this.edition += newValue - 2004;
+			}
+		}
+	}
 });
 
-console.log(person.name);
-person.name = "nihaoma";
-console.log(person.name);
+var descriptor = Object.getOwnPropertyDescriptor(book,'_year');
+//由于其获取的是一个数据属性，所以其含有四个属性 value configurable writable enumrabl
+console.log(descriptor.value);
+console.log(descriptor.configurable);
+
+
+console.log(typeof descriptor.get); 
+
+var descriptor = Object.getOwnPropertyDescriptor(book,'year');
+console.log(descriptor.value);
+console.log(descriptor.enumerable);
+console.log(typeof descriptor.get);
