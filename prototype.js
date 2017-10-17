689fcbb681c5a27fd71c9c7e44f44262f5ac2790
suifeng123 <wswen1220@163.com>
2017-10-17 08:11:59 +0800

    提交关于prototype的属性

---
+++ prototype.js
@@ -0,0 +1,25 @@
+//书写一个原型模式
+function  Person(){}
+	Person.prototype.name = "wangshengwen";
+	Person.prototype.age = 29;
+	Person.prototype.job = "Software Engineer";
+
+	Person.prototype.sayName = function(){
+		console.log(this.name);
+	};
+
+var person1 = new Person();
+person1.sayName();
+
+var person2 = new Person();
+person2.sayName();
+
+console.log(person1.sayName == person2.sayName);
+
+console.log('isPrototypeOf');
+console.log(Person.prototype.isPrototypeOf(person1));
+console.log(Person.prototype.isPrototypeOf(person2));
+
+console.log('getPrototypeOf');
+console.log(Object.getPrototypeOf(person1) == Person.prototype);
+console.log(Object.getPrototypeOf(person2) === Person.prototype);
