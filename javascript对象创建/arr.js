bf1d972e3577f7892655518adf347e47aed1ce1d
suifeng123 <wswen1220@163.com>
2017-12-01 09:14:26 +0800

    提交js中数组的部分形式

---
+++ javascript对象创建/arr.js
@@ -0,0 +1,10 @@
+//javaScript关于数组的算法
+var arr = [1,2,3,4,5,6,7,8,9,10];
+//将数组当成一个队列来看 有两个算法 shift unshift
+console.log(arr.shift()); //并且这个算法改变了原来的数组的形式
+console.log(arr);
+
+arr.unshift(1);
+console.log(arr);
+
+
