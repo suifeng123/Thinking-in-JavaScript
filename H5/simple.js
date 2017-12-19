52e4325626573e891c438d323908866c0956d79a
suifeng123 <wswen1220@163.com>
2017-12-19 14:36:07 +0800

    lll

--- H5/simple.js
+++ H5/simple.js
@@ -1,2 +1,12 @@
 //此处有一个
-document.getElementById('applecode').innerText = "sadfsadf";
+//document.getElementById('applecode').innerText = "sadfsadf";
+function updateUI(){
+   var imgs = document.getElementsByTagName('img');
+   for(var i = 0,len=imgs.length; i < len ; i++){
+       imgs[i].title = document.title + " image " + i;
+   }
+
+   var msg = document.getElementById('msg');
+   msg.innerHTML = "Update complete";
+}
+
