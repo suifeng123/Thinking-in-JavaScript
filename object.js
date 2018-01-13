3b63ec504ecc1a5e7753efc6c38011a855005860
suifeng123 <wswen1220@163.com>
2018-01-13 15:45:15 +0800

    提交对象封装

---
+++ object.js
@@ -0,0 +1,8 @@
+var a = new String('abc');
+var b = new Number(42);
+var c = new Boolean(true);
+
+
+console.log(a.valueOf()); //"abc"
+console.log(b.valueOf()); //42
+console.log(c.valueOf());//true
