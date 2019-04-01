f69fd0bbf1c2d0a060c273d17b3123de042a643c
王圣文 <wswen1220@163.com>
2019-04-01 18:56:58 +0800

    提交闭包的更新

--- object_javascript/this.js
+++ object_javascript/this.js
@@ -10,52 +10,19 @@
 /**
 *  this 作为构造器进行使用
 */
-var MyClass = function(){
-	this.name = "sven";
-	return 
-		name: 'anne'
-	
-}
 
-var obj = new MyClass();
-console.log(obj.name);
 /**
  * this的第四种使用方法
  * Function.prototype.call 或者
  * Function.prototype.apply
  */
-
-var obj1 = {
-	name: 'sven',
-	getName: function(){
-		return this.name;
-	}
-};
-
-var obj2 = {
-	name: 'anne'
-};
-
-console.log(obj1.getName());//输出: sven 
-console.log(obj1.getName.call(obj2));//输入anne  
-
-Function.prototype.bind = function(context){
-	/**
-	 * 其中context 是指函数方法指向的方向  
-	 * 就是说 最终函数指向最终的方向是这个环境
-	 */
-	var self = this;//保存原函数
-	return function(){ //进行返回一个函数
-		return self.apply(context,arguments);
-	}
-};
-
-var obj = {
-	name: 'sven'
-};
-
-var func = function(){
-	console.log(this.name);
-}.bind(obj);
-
-func();
+  var Type = {};
+  for(var i = 0,type;type=['String','Array','Number'][i++];){
+	  Type['is' + type] = function(obj){
+		  return Object.prototype.toString.call(obj) === '[object' + type+']';
+	  }
+  }
+  
+  console.log(Type.isArray([]));
+  console.log(Type.isString("str"));
+  console.log(Type.isNumber("4"));
