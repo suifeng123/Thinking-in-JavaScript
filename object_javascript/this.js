26f80a5285300a039579e795919c8efd57809d22
王圣文 <wswen1220@163.com>
2019-04-01 18:32:51 +0800

    提交javascript 中this的用法

--- object_javascript/this.js
+++ object_javascript/this.js
@@ -7,10 +7,55 @@
 /**
  * 首先是作为对象的方法使用
  */
-var obj = {
-	a: 1,
-	getA: function(){
-		console.log(this === obj);
-		
-	}
+/**
+*  this 作为构造器进行使用
+*/
+var MyClass = function(){
+	this.name = "sven";
+	return 
+		name: 'anne'
+	
 }
+
+var obj = new MyClass();
+console.log(obj.name);
+/**
+ * this的第四种使用方法
+ * Function.prototype.call 或者
+ * Function.prototype.apply
+ */
+
+var obj1 = {
+	name: 'sven',
+	getName: function(){
+		return this.name;
+	}
+};
+
+var obj2 = {
+	name: 'anne'
+};
+
+console.log(obj1.getName());//输出: sven 
+console.log(obj1.getName.call(obj2));//输入anne  
+
+Function.prototype.bind = function(context){
+	/**
+	 * 其中context 是指函数方法指向的方向  
+	 * 就是说 最终函数指向最终的方向是这个环境
+	 */
+	var self = this;//保存原函数
+	return function(){ //进行返回一个函数
+		return self.apply(context,arguments);
+	}
+};
+
+var obj = {
+	name: 'sven'
+};
+
+var func = function(){
+	console.log(this.name);
+}.bind(obj);
+
+func();
