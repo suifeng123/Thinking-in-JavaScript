234fc7d44126c3efab950081066dce67ccdc2426
王圣文 <wswen1220@163.com>
2019-04-08 15:05:49 +0800

    提交路径的提交的VueRouter

--- ES6/single.js
+++ ES6/single.js
@@ -7,3 +7,38 @@ var Singleton = function(name){
 Singleton.prototype.getName = function(){
 	console.log(this.name);
 };
+
+Singleton.getInstance = function(name){
+	if(!this.instance){
+		//如果不能找不到相应的单例的话 就创造一个相应的单例模式
+		this.instance = new Singleton(name);
+	}
+	//如果已经具有此种实例 就返回这个实例
+	return this.instance;
+};
+
+var a = Singleton.getInstance('sven1');
+var b = Singleton.getInstance('sven2');
+
+console.log("进行相应的验证");
+console.log(a === b);
+
+//or 
+var Singleton = function(name){
+	this.name = name;
+};
+
+Singleton.prototype.getName = function(){
+	console.log(this.name);
+};
+
+Singleton.getInstance = (function(){
+	var instance = null;
+	return function(name){
+		if(!instance){
+			instance = new Singleton(name);
+		}
+		
+		return instance;
+	}
+})();
--- vueRouter/vueRouter.js
+++ vueRouter/vueRouter.js
@@ -46,8 +46,151 @@ export function install(Vue){
 				this._router = this.$options.router 
 				//初始化路由
 				this._router.init(this);
-				//
+				//很重要 为_router 属性实现双向绑定
+				//触发组件渲染
+				Vue.util.defineReative(this,'_route',this._router.history.current)
+			}else{
+				//用于router-view层级判断
+				this._routerRoot = (this.$parent && this.$parent._routerRoot) || this 
 			}
+			registerInstance(this,this);
+		},
+		destroyed(){
+			registerInstance(this)
 		}
 	})
+	
+	//全局注册组件 router-link 和 router-view
+	Vue.component('RouterView',View);
+	Vue.component('RouterLink',Link);
+	
+}
+
+/**
+ * 在安装插件后，对VueRouter进行实例化
+ */
+const Home = { template: '<div>Home</div>' };
+const Foo = { template: '<div>foo</div>' };
+const Bar = { template: '<div>bar</div>' };
+
+//3.Create the router 
+const router = new VueRouter({
+	mode:'hash',
+	base: __dirname,
+	routes:[
+		{path:'/',component: Home},
+		{path:'/foo',component: Foo},
+		{path:'/bar',component: Bar}
+	]
+})
+
+//construction 构造函数
+constructor(options: RouterOptions = {}){
+	//...
+	//路由匹配对象
+	this.matcher = createMatcher(options.routes || [],this);
+	
+	//根据mode采取不同的路由方式
+	let mode = options.mode || 'hash';
+	this.fallback = mode === 'history' && !supportsPushState
+	&& options.fallback !== false 
+	if(this.fallback){
+		mode = 'hash';
+	}
+	if(!inBrowser){
+		mode = 'abstract';
+	}
+	this.mode = mode;
+	
+	switch(mode){
+		case 'history':
+		   this.history = new HTML5History(this,options.base);
+		   break;
+		case 'hash':
+		   this.history = new HashHistory(this,options.base,this.fallback);
+		   break;
+		case 'abstract':
+		   this.history = new AbstractHistory(this,options.base);
+		   break;
+		default:
+		   if(process.env.NODE_ENV !== 'production'){
+			   assert(false,`invalid mode: ${mode}`)
+		   }
+	}
+}
+
+export function createMatcher(
+    routes: Array<RouteConfig>,
+	router: VueRouter
+): Matcher{
+	//创建路由映射表
+	const { pathList,pathMap,nameMap } = createRouteMap(routes);
+	
+	function addRoutes(routes){
+		createRouteMap(routes,pathList,pathMap,nameMap);
+	}
+	
+	//路由匹配
+	function match(
+	  raw: RawLocation,
+	  currentRoute?: Route,
+	  redirectedForm?: Location
+	): Route{
+		//... 
+	}
+	
+	return {
+		match,
+		addRoutes 
+	}
+}
+
+export function createRouteMap(
+   routes: Array<RouteConfig>,
+   oldPathList?: Array<string>,
+   oldPathMap?: Dictionary<RouteRecord>,
+   oldNameMap?: Dictionary<RouteRecord>
+):{
+	pathList: Array<string>;
+	pathMap: Dictionary<RouteRecord>;
+	nameMap: Dictionary<RouteRecord>;
+}{
+	//创建相应的映射表
+	const pathList: Array<string> = oldPathList || [],
+	const pathMap: Dictionary<RouteRecord> = oldPathMap || object.create(null)
+	const nameMap: Dictionary<RouteRecord> = oldNameMap || object.create(null)
+	//遍历路由配置 为每个配置添加路由记录
+	routes.forEach(route => {
+		addRouteRecord(pathList,pathMap,nameMap,route)
+	})
+	
+	//确保通配符在最后
+	for(let i = 0, l = pathList.length; i < l; i++){
+		if(pathList[i] === '*'){
+			pathList.push(pathList.splice(i,1)[0]);
+			l--;
+			i--;
+		}
+	}
+	return {
+		pathList,
+		pathMap,
+		nameMap
+	}
+}
+
+//添加路由记录
+function addRouteRecord(
+   pathList: Array<string>,
+   pathMap: Dictionary<RouteRecord>,
+   nameMap: Dictionary<RouteRecord>,
+   route:RouteConfig,
+   parent?:RouteRecord,
+   matchAs?:string 
+){
+	//获得路由配置下的属性
+	const { path,name } = route 
+	const pathToRegexpOptions: PathToRegexpOptions = route.pathToRegexpOptions || {},
+	//格式化 url, 替换
+	const normalizedPath = normalizePath(path,parent,pathToRegexpOptions.strict)
 }
