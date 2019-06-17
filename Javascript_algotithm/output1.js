d5e2cb57fe915bdf53007948bd05f6f0ccf64caf
suifeng123 <wswen1220@163.com>
2019-06-17 17:24:23 +0800

    提交主线

--- Javascript_algotithm/output1.js
+++ Javascript_algotithm/output1.js
@@ -1,3 +1,5 @@
+
+/**
 function sayHi(){
 	console.log(name);
 	console.log(age);
@@ -6,3 +8,14 @@ function sayHi(){
 }
 
 sayHi();
+**/
+const shape = {
+	radius:10,
+	diameter(){
+		return  this.radius * 2
+	},
+	perimeter: () => 2 * Math.PI * this.radius
+};
+
+console.log(shape.diameter()); //进项返回半径的操作
+console.log(shape.perimeter()); //进行面积的计算
