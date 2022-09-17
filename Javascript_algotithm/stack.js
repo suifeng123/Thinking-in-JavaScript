352a812923c5efc4dc66d80fe015c94905a6f892
wangshengwen <wswen1220@163.com>
2022-09-17 20:21:38 +0800

    提交一个进制的算法

--- Javascript_algotithm/stack.js
+++ Javascript_algotithm/stack.js
@@ -9,7 +9,7 @@ class Stack {
 	}
 	
 	pop() {
-		this.items.pop()
+		return this.items.pop()
 	}
 	
 	peek() {
@@ -33,4 +33,64 @@ class StackObj {
 		this.items = {}
 		this.count = 0
 	}
-}
+	push(element) {
+		this.items[this.count] = element
+		this.count++
+	}
+	size() {
+		return this.count
+	}
+	isEmpty() {
+		return this.count == 0
+	}
+	pop() {
+		if (this.isEmpty()) {
+			return undefined;
+		}
+		this.count--
+		const result = this.items[this.count]
+		delete this.items[this.count]
+		return result;
+	}
+	peek() {
+		if (this.isEmpty()) {
+			return undefined;
+		}
+		return this.items[this.count - 1]
+	}
+	clear() {
+		this.items = {}
+		this.count = 0
+	}
+	
+	toString() {
+		if (this.isEmpty()) {
+			return ""
+		}
+		let objString = `${this.items[0]}`;
+		for (let i = 1; i < this.count; i++) {
+			objString = `${objString}, ${this.items[i]}`
+		}
+		return objString
+	}
+ }
+ 
+ 
+ function decimalToBinary(decNumber) {
+	 var stack = new Stack()
+	 let number = decNumber;
+	 let rem;
+	 let binaryString = ''
+	 while (number > 0) {
+		 rem = Math.floor(number % 2)
+		 stack.push(rem)
+		 number = Math.floor(number / 2)
+	 }
+	 console.log(stack)
+	 while (!stack.isEmpty()) {
+		 binaryString += stack.pop()
+	 }
+	 return binaryString
+ }
+ 
+ console.log(decimalToBinary(233))
